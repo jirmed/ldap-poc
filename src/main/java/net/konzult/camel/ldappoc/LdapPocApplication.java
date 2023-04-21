dd0aa2c1daf4783bcdb2d0afaf2e23f3ad538bf3
Jiri Medved <jmedved@bvv.cz>
2023-04-21 11:59:01 +0200

    Initial commit

---
+++ src/main/java/net/konzult/camel/ldappoc/LdapPocApplication.java
@@ -0,0 +1,13 @@
+package net.konzult.camel.ldappoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/konzult/camel/ldappoc/LdapPocApplicationTests.java
@@ -0,0 +1,13 @@
+package net.konzult.camel.ldappoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
